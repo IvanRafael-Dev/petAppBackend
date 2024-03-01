03c3a62d7537300a9eecfc3d7ec0b3e3d673f8c0
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2024-03-01 00:00:08 -0300

    Add user and profile endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 const app = express();
 app.use(express.json());
@@ -11,6 +10,55 @@ const prisma = new PrismaClient();
 
 app.get('/', (req, res) => {
   res.send('Hello Pets!');
+});
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      createdAt: true,
+      updatedAt: true,
+      Profile: {
+        select: {
+          id: true,
+          bio: true,
+          phone: true,
+          createdAt: true,
+          updatedAt: true
+        }
+      }
+    }
+  });
+  return res.json(users);
+});
+
+app.post('/profiles', async (req, res) => {
+  const { bio, userId, phone } = req.body;
+
+  try {
+    const profile = await prisma.profile.create({
+      data: {
+        bio,
+        phone,
+        userId
+      },
+      select: {
+        id: true,
+        bio: true,
+        phone: true,
+        createdAt: true,
+        updatedAt: true
+      }
+    });
+
+    return res.status(201).json(profile);
+  } catch (error) {
+    return res.status(500).json({ error: (error as Error).message });
+  } finally {
+    await prisma.$disconnect();
+  }
 });
 
 app.post('/users', async (req, res) => {
@@ -38,6 +86,13 @@ app.post('/users', async (req, res) => {
   } finally {
     await prisma.$disconnect();
   }
+});
+
+process.on('SIGINT', async () => {
+  console.log('\nSIGINT: Closing database connection');
+  await prisma.$disconnect();
+  console.log('Server is shutting down...');
+  process.exit(0);
 });
 
 export { app };
