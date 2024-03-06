04baa1b7ce3a9e2e5422750bef26be3d0b3c1672
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2024-03-06 10:22:49 -0300

    feat: signup controller returns 400 if username is not provided

---
+++ src/presentation/controllers/SignUpController.ts
@@ -0,0 +1,16 @@
+import { type Controller } from './interfaces/Controller';
+import { type HttpRequest, type HttpResponse } from './interfaces/Http';
+
+export class SignUpController implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse | any> {
+    const requiredFields = ['username'];
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return {
+          statusCode: 400,
+          body: new Error(`Missing param: ${field}`)
+        };
+      }
+    }
+  }
+}
