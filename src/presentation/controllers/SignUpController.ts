268bffa3f94add75d20d0a0be862f3f33a3d0d94
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2024-03-06 10:29:17 -0300

    feat: signup controller returns 400 if password is not provided

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -3,7 +3,7 @@ import { type HttpRequest, type HttpResponse } from './interfaces/Http';
 
 export class SignUpController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse | any> {
-    const requiredFields = ['username', 'email'];
+    const requiredFields = ['username', 'email', 'password'];
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return {
