a9982e27d9015e02aab4baeaf825609688c00655
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2024-03-06 10:28:40 -0300

    test: signup controller returns 400 if email is not provided

--- src/presentation/controllers/SignUpController.test.ts
+++ src/presentation/controllers/SignUpController.test.ts
@@ -1,9 +1,9 @@
 import { SignUpController } from './SignUpController';
 import { type HttpRequest } from './interfaces/Http';
 
-type FakeHttpReqesutFields = 'username' | 'email' | 'password' | 'passwordConfirmation';
+type FakeHttpRequestFields = 'username' | 'email' | 'password' | 'passwordConfirmation';
 
-const makeHttpRequestBody = (missingField?: FakeHttpReqesutFields): HttpRequest => {
+const makeHttpRequestBody = (missingField?: FakeHttpRequestFields): HttpRequest => {
   const body = {
     username: 'any_name',
     email: 'any_email@mail.com',
@@ -15,8 +15,8 @@ const makeHttpRequestBody = (missingField?: FakeHttpReqesutFields): HttpRequest 
     return { body };
   }
 
-  const { [missingField]: _, ...newBody } = body;
-  return { body: newBody };
+  const { [missingField]: _, ...outputBody } = body;
+  return { body: outputBody };
 };
 
 describe('#SignUpController', () => {
@@ -27,5 +27,14 @@ describe('#SignUpController', () => {
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error('Missing param: username'));
+  });
+
+  it('should return 400 if no email is provided', async () => {
+    const sut = new SignUpController();
+    const httpRequest = makeHttpRequestBody('email');
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'));
   });
 });
