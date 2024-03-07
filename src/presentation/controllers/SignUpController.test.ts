3f609693bb1996cad1e5f94c0265e448bf4b8c1c
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2024-03-06 22:12:53 -0300

    Add validation tests to SignUpController

--- src/presentation/controllers/SignUpController.test.ts
+++ src/presentation/controllers/SignUpController.test.ts
@@ -1,3 +1,4 @@
+import { type Validation } from '../validators/interfaces/Validation';
 import { SignUpController } from './SignUpController';
 import { type HttpRequest } from './interfaces/Http';
 
@@ -19,40 +20,85 @@ const makeHttpRequestBody = (missingField?: FakeHttpRequestFields): HttpRequest 
   return { body: outputBody };
 };
 
-describe('#SignUpController', () => {
-  it('should return 400 if no name is provided', async () => {
-    const sut = new SignUpController();
-    const httpRequest = makeHttpRequestBody('username');
+interface SutTypes {
+  sut: SignUpController
+  validationStub: Validation
+}
 
+const makeValidationStub = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error | void {}
+  }
+
+  return new ValidationStub();
+};
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidationStub();
+  const signUpController = new SignUpController(validationStub);
+  return {
+    sut: signUpController,
+    validationStub
+  };
+};
+
+describe('#SignUpController', () => {
+  it('should call validation with correct values', async () => {
+    const { sut, validationStub } = makeSut();
+    const validationSpy = jest.spyOn(validationStub, 'validate');
+    const httpRequest = makeHttpRequestBody();
+    await sut.handle(httpRequest);
+    expect(validationSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+
+  it('should return 400 if no username is provided', async () => {
+    const { sut, validationStub } = makeSut();
+
+    jest.spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new Error('Missing param: username'));
+
+    const httpRequest = makeHttpRequestBody('username');
     const httpResponse = await sut.handle(httpRequest);
+
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error('Missing param: username'));
+    expect(httpResponse.body).toEqual({ message: 'Missing param: username' });
   });
 
   it('should return 400 if no email is provided', async () => {
-    const sut = new SignUpController();
+    const { sut, validationStub } = makeSut();
+
+    jest.spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new Error('Missing param: email'));
     const httpRequest = makeHttpRequestBody('email');
 
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error('Missing param: email'));
+    expect(httpResponse.body).toEqual({ message: 'Missing param: email' });
   });
 
   it('should return 400 if no password is provided', async () => {
-    const sut = new SignUpController();
+    const { sut, validationStub } = makeSut();
+
+    jest.spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new Error('Missing param: password'));
+
     const httpRequest = makeHttpRequestBody('password');
 
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error('Missing param: password'));
+    expect(httpResponse.body).toEqual({ message: 'Missing param: password' });
   });
 
   it('should return 400 if no password confirmation is provided', async () => {
-    const sut = new SignUpController();
+    const { sut, validationStub } = makeSut();
+
+    jest.spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new Error('Missing param: passwordConfirmation'));
+
     const httpRequest = makeHttpRequestBody('passwordConfirmation');
 
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error('Missing param: passwordConfirmation'));
+    expect(httpResponse.body).toEqual({ message: 'Missing param: passwordConfirmation' });
   });
 });
