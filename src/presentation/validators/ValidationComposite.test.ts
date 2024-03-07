f6c3ffc9f165b053ac2dbe300b0c5c4588d5ba7a
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2024-03-06 22:12:03 -0300

    Add ValidationComposite test

---
+++ src/presentation/validators/ValidationComposite.test.ts
@@ -0,0 +1,40 @@
+import { ValidationComposite } from './ValidationComposite';
+import { type Validation } from './interfaces/Validation';
+
+const makeValidationStub = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error | void {}
+  }
+
+  return new ValidationStub();
+};
+
+interface SutTypes {
+  sut: ValidationComposite
+  validationStubs: Validation[]
+}
+
+const makeSut = (): SutTypes => {
+  const validationStubs = [makeValidationStub(), makeValidationStub()];
+  const sut = new ValidationComposite(validationStubs);
+  return { sut, validationStubs };
+};
+
+describe('#ValidationComposite', () => {
+  it('should return an error if any validation fails', () => {
+    const { sut, validationStubs } = makeSut();
+    const error = new Error('any_error');
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(error);
+    const response = sut.validate({ field: 'any_value' });
+    expect(response).toEqual(error);
+  });
+
+  it('should return the first error if more than one validation fails', () => {
+    const { sut, validationStubs } = makeSut();
+    const error = new Error('first_error');
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(error);
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new Error('second_error'));
+    const response = sut.validate({ field: 'any_value' });
+    expect(response).toEqual(error);
+  });
+});
