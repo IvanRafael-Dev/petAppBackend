1a2ec2df44dca3e0a51eae3b3a946be49f8075e6
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2024-03-06 22:13:36 -0300

    Add makeSignUpController and makeValidationComposite functions

---
+++ src/main/factorys/makeSignUpController.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from '../../presentation/controllers/SignUpController';
+import { RequiredFieldsValidation } from '../../presentation/validators/RequiredFieldsValidation';
+import { ValidationComposite } from '../../presentation/validators/ValidationComposite';
+
+export const makeValidationComposite = (): ValidationComposite => {
+  const validationComposite = new ValidationComposite([]);
+  return validationComposite;
+};
+
+export const makeSignUpController = (): SignUpController => {
+  const validationComposite = makeValidationComposite();
+  const requiredFields = new RequiredFieldsValidation(['name', 'email', 'password', 'passwordConfirmation']);
+  validationComposite.add(requiredFields);
+  const signUpController = new SignUpController(validationComposite);
+  return signUpController;
+};
