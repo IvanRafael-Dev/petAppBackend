0063e3c8db20ba15bf016b4a87d6b6e94e93de3b
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2024-03-07 11:10:09 -0300

    Add EmailValidation and EmailValidator interfaces

---
+++ src/presentation/utils/email/EmailValidation.ts
@@ -0,0 +1,18 @@
+import { InvalidParamError } from '../../errors/invalidParamError';
+import { type Validation } from '../../validators/interfaces/Validation';
+import { type EmailValidator } from './interface/EmailValidator';
+
+export class EmailValidation implements Validation {
+  private readonly emailValidator: EmailValidator;
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
+  validate (input: Record<string, string>): Error | void {
+    const isValid = this.emailValidator.isValid(input.email);
+    if (!isValid) {
+      return new InvalidParamError('email');
+    }
+  }
+}
