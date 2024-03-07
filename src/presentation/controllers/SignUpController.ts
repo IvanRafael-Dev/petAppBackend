486d61e36c4e33c2edc4f2d842bcc64506f1c065
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2024-03-06 22:12:39 -0300

    Add validation to SignUpController

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,16 +1,23 @@
+import { type Validation } from '../validators/interfaces/Validation';
 import { type Controller } from './interfaces/Controller';
 import { type HttpRequest, type HttpResponse } from './interfaces/Http';
 
 export class SignUpController implements Controller {
+  private readonly validation: Validation;
+
+  constructor (validation: Validation) {
+    this.validation = validation;
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse | any> {
-    const requiredFields = ['username', 'email', 'password', 'passwordConfirmation'];
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return {
-          statusCode: 400,
-          body: new Error(`Missing param: ${field}`)
-        };
-      }
+    const error = this.validation.validate(httpRequest.body);
+    if (error) {
+      return {
+        statusCode: 400,
+        body: {
+          message: error.message
+        }
+      };
     }
   }
 }
