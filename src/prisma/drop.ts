165f41ab90da3a944fdd015936a78adef59a7ef2
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2024-03-10 18:30:27 -0300

    fix: error when droping a inexistent database

--- src/prisma/drop.ts
+++ src/prisma/drop.ts
@@ -1,9 +1,21 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_PORT
+} = process.env;
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: `mysql://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/mysql`
+    }
+  }
+});
 
-async function main (): Promise<void> {
-  await prisma.$executeRawUnsafe('DROP DATABASE IF EXISTS pet_app;');
+async function main (): Promise<void | null> {
+  await prisma.$queryRawUnsafe('DROP DATABASE pet_app;');
 }
 
 main()
@@ -11,8 +23,11 @@ main()
     console.log('Database dropped');
   })
   .catch((e) => {
-    console.error(e);
-    process.exit(1);
+    if (e instanceof PrismaClientKnownRequestError) {
+      console.error(e.message);
+      return;
+    }
+    console.error('Database does not exist');
   })
   .finally(async () => {
     await prisma.$disconnect();
