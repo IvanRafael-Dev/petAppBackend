877c41e0811240a672aeefd8b579a7fa03ea6ef4
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2024-03-19 16:32:01 -0300

    refactor: handle method return type

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -11,7 +11,7 @@ export class SignUpController implements Controller {
     this.validation = validation;
   }
 
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse | any> {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(httpRequest.body);
     if (error) {
       return badRequest(error);
